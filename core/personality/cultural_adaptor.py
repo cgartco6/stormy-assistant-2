9c2ce738d4c537b56fb01b5035f46abe859f2711
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-03-15 18:50:35 +0200

    Create cultural_adaptor.py

---
+++ core/personality/cultural_adaptor.py
@@ -0,0 +1,7 @@
+class CulturalAdaptor:
+    def __init__(self, language="en_ZA"):
+        self.language = language
+
+    def adapt_response(self, response, mood):
+        # Placeholder for cultural adaptation
+        return response
